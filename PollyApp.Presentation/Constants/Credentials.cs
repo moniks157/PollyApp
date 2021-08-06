3688c441dadbb7639f64d195c38114703de9d86f
Monika Maczkowiak <mm@maczek.net>
2021-08-06 13:07:55 +0200

    + reauthorise policy

---
+++ PollyApp.Presentation/Constants/Credentials.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PollyApp.Presentation.Constants
+{
+    public static class Credentials
+    {
+        public const string API_KEY = "key=4d353833-036f-48e3-9126-d79cb386ec2d";
+        public const string WRONG_API_KEY = "key=1234";
+    }
+}
---
+++ PollyApp.Presentation/Constants/HolidaysUriConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PollyApp.Presentation.Constants
+{
+    public static class HolidaysUriConstants
+    {
+        public const string HOLIDAYS = "holidays?";
+        public const string COUNTRY_PL = "country=PL";
+        public const string YEAR_2021 = "year=2020";
+    }
+}
